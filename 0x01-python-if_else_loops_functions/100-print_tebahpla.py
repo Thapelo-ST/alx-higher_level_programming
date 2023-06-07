b6c2f3be5f6395f8f97e95ea6fb192c274ce802e
Thapelo-ST <tsamkelo96@gmail.com>
2023-06-07 16:17:54 -0700

    tebahpla

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 for i in range(ord('Z'), ord('A') - 1, -1):
     print("{}{}".format(chr(i + 32 if (ord('Z') - i) % 2 == 0 else i),
                         chr(i)), end='')
