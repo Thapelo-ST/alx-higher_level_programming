66253ec53c378f9906676426a9490b9cc24a7f54
Thapelo-ST <tsamkelo96@gmail.com>
2023-06-14 00:30:54 +0000

    final tasks

---
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+import string
+_ = print(*(string.ascii_uppercase), sep='')
---
+++ 0x02-python-import_modules/test.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+from 102-magic_calculation import magic_calculation
+
+result = magic_calculation(10, 5)
+print(result)
