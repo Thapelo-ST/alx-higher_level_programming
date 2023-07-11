1db2516151276f69aff2cdb28464e8aa7befcb72
Taps <105137490+Thapelo-ST@users.noreply.github.com>
2023-07-11 20:39:38 +0200

    Update 4-inherits_from.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -2,7 +2,7 @@
 """
 that returns True if the object is an instance of a class
 that inherited (directly or indirectly)
-from the specified class ; otherwise False.
+in the specified class ; otherwise False.
 """
 
 
