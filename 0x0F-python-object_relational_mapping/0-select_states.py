5f4b36ea503d61fb28ac2a742dd51bdcabdc6d73
Thapelo-ST <tsamkelo96@gmail.com>
2023-09-15 12:30:02 +0200

    check

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-import MySQLdb
 import sys
+import MySQLdb
 """
     list_states a module or function that prints all states by id order
 """
@@ -10,41 +10,29 @@ def list_states(mysql_username, mysql_password, database_name):
     """
         list_states a module or function that prints all states by id order
     """
-    try:
-        connection = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=mysql_username,
-            passwd=mysql_password,
-            db=database_name
-        )
+    connection = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=mysql_username,
+        passwd=mysql_password,
+        db=database_name
+    )
 
-        cursor = connection.cursor()
+    cursor = connection.cursor()
 
-        cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
-        rows = cursor.fetchall()
+    rows = cursor.fetchall()
 
-        for row in rows:
-            print(row)
+    for row in rows:
+        print(row)
 
-    except MySQLdb.Error as e:
-        print(f"Error: {e}")
-    finally:
-        if 'cursor' in locals():
-            cursor.close()
-        if 'connection' in locals():
-            connection.close()
+    cursor.close()
+    connection.close()
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: python list_states.py "
-              "<mysql_username> <mysql_password> <db_name>")
-        sys.exit(1)
-
     mysql_username = sys.argv[1]
     mysql_password = sys.argv[2]
     database_name = sys.argv[3]
-
     list_states(mysql_username, mysql_password, database_name)
