05adf7d30594b57dfa4b287656a4b8e5c3efea8c
Thapelo-ST <tsamkelo96@gmail.com>
2023-09-15 19:07:10 +0200

    filter cities fix

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -25,7 +25,7 @@ if __name__ == "__main__":
 
         query = """
             SELECT cities.name FROM cities
-            JOIN states ON cities.state_id = states.id 
+            JOIN states ON cities.state_id = states.id
             WHERE states.name = %s
             ORDER BY cities.id ASC
         """
