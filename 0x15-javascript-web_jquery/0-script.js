d778f271887c6fd7b79b5bf095a9262ec4e9c903
Thapelo-ST <tsamkelo96@gmail.com>
2023-11-01 22:29:24 +0200

    test 1

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const headerElement = document.querySelector('header');
+
+  if (headerElement) {
+    headerElement.style.color = '#FF0000';
+  } else {
+    console.error('Error');
+  }
+});
